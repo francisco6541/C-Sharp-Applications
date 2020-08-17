f10733de09411e344d2990a4ac12b0440c4001e9
Francisco Velazquez <francisco.velazquez2@outlook.com>
2020-08-17 15:52:34 -0400

    Add files via upload

---
+++ ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Class:        ISTA220 Programming Fundamentals
+Student:      Francisco Velazquez
+Instructor:   Christine Lee
+Date:         08/14/2020
+Descrption:   This application is an example of how to use extension methods.
+Revised:      
+Revision By:  
+Revision:     
+*/
+
+using System;
+using Extensions;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void doWork()
+        {
+            int x = 591;
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.WriteLine($"{x} in base {i} is {x.ConvertToBase(i)}");
+            }
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ Vehicles/Vehicles/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Class:        ISTA220 Programming Fundamentals
+Student:      Francisco Velazquez
+Instructor:   Christine Lee
+Date:         08/14/2020
+Descrption:   This application is an example of polymorphism.
+Revised:      
+Revision By:  
+Revision:     
+*/
+
+using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("Journey by airplane:");
+            Airplane myPlane = new Airplane();
+            myPlane.StartEngine("Contact");
+            myPlane.TakeOff();
+            myPlane.Drive();
+            myPlane.Land();
+            myPlane.StopEngine("Whirr");
+
+            Console.WriteLine();
+            Console.WriteLine("Journey by car:");
+            Car myCar = new Car();
+            myCar.StartEngine("Brm brm");
+            myCar.Accelerate();
+            myCar.Drive();
+            myCar.Brake();
+            myCar.StopEngine("Phut phut");
+
+            Console.WriteLine("\nTesting polymorphism");
+            Vehicle v = myCar;
+            v.Drive();
+            v = myPlane;
+            v.Drive();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
