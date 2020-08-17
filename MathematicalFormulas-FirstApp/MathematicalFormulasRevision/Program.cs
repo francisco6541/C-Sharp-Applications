197bb856f17f7ddab5ea8449be3f2009bd62dc4a
Francisco Velazquez <francisco.velazquez2@outlook.com>
2020-08-17 15:59:15 -0400

    Add files via upload

---
+++ MathematicalFormulas-FirstApp/MathematicalFormulasRevision/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalFormulasRevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1
+            Console.WriteLine("Hello Christine! Lets get started with some math!");
+            Console.WriteLine("Part 1, circumference and area of a circle.");
+            Console.Write("Enter an integer for the radius: ");
+            string strradius = Console.ReadLine();
+            int intradius = int.Parse(strradius);
+            double circumference = 2 * Math.PI * intradius;
+            Console.WriteLine($"The circumference is {circumference}");
+            double area = Math.PI * Math.Pow(intradius, 2);
+            Console.WriteLine($"The area is {area}");
+
+            //Part 2
+            Console.WriteLine("Part 2, volume of a hemisphere.");
+            Console.Write("Enter an integer for the radius: ");
+            string strradius2 = Console.ReadLine();
+            int intradius2 = int.Parse(strradius2);
+            double volume = (1.3333333333 * Math.PI * Math.Pow(intradius2, 3)) / 2;
+            Console.WriteLine($"The volume is {volume}");
+
+            //Part 3
+            Console.WriteLine("Part 3, area of a triangle (Heron's formula).");
+            Console.Write("Enter an integer for side a: ");
+            string strsidea = Console.ReadLine();
+            int intsidea = int.Parse(strsidea);
+            Console.Write("Enter an integer for side b: ");
+            string strsideb = Console.ReadLine();
+            int intsideb = int.Parse(strsideb);
+            Console.Write("Enter an integer for side c: ");
+            string strsidec = Console.ReadLine();
+            int intsidec = int.Parse(strsidec);
+            double semiperimeter = (intsidea + intsideb + intsidec) / 2.0;
+            double area2 = Math.Sqrt(semiperimeter * (semiperimeter - intsidea) * (semiperimeter - intsideb) * (semiperimeter - intsidec));
+            Console.WriteLine($"The area is {area2}");
+
+            //Part 4
+            Console.WriteLine("Part 4, solving a quadratic equation.");
+            Console.Write("Enter an integer for a: ");
+            string stra = Console.ReadLine();
+            int a = int.Parse(stra);
+            Console.Write("Enter an integer for b: ");
+            string strb = Console.ReadLine();
+            int b = int.Parse(strb);
+            Console.Write("Enter an integer for c: ");
+            string strc = Console.ReadLine();
+            int c = int.Parse(strc);
+            double sqrtpart = Math.Sqrt((b * b) - (4 * a * c));
+            double x1 = (-b + sqrtpart) / (2 * a);
+            double x2 = (-b - sqrtpart) / (2 * a);
+            Console.WriteLine($"The positive solution is {x1}");
+            Console.WriteLine($"The negative solution is {x2}");
+            Console.WriteLine("That was quite the jump from Hello World! but I hope you enjoyed my first little program!");
+        }
+    }
+}
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Class:        ISTA220 Programming Fundamentals
+Student:      Francisco Velazquez
+Instructor:   Christine Lee
+Date:         08/07/2020
+Descrption:   This application demonstrates using constuctors and using static vs non static methods.
+Revised:      
+Revision By:  
+Revision:     
+*/
+
+#region using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Parameters
+{
+    class Program
+    {
+        static void doWork()
+        {
+            int i = 0;
+            Console.WriteLine(i);
+            Pass.value(ref i);
+            Console.WriteLine(i);
+            WrappedInt wi = new WrappedInt();
+            Console.WriteLine(wi.Number);
+            Pass.Reference(wi);
+            Console.WriteLine(wi.Number);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
