b31e23e7554480ecac193712eda0c3f568dd238a
Francisco Velazquez <francisco.velazquez2@outlook.com>
2020-08-17 15:45:21 -0400

    Add files via upload

---
+++ IndividualWorkPT2_FranciscoVelazquez07222020/Program.cs
@@ -0,0 +1,53 @@
+﻿//Class:      ISTA 220 Programming Fundamentals
+//Student:    Francisco Velazquez
+//Instructor: Christine E Lee, Instructor
+//Date:       07/22/2020
+
+/*********************************************************
+
+This project has one method that asks for the users name and another method that calculates the area of a rectangle.
+
+Revisions:
+
+
+Revision Date:
+
+
+Revised By:
+
+
+********************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualWorkPT2_FranciscoVelazquez07222020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user! What is you first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine($"Hello {firstName}! What is your last name?");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"So your name is {firstName} {lastName}. That's a nice name!");
+            Console.ReadLine();
+            Program p = new Program();
+            p.secondMethod();
+        }
+        public void secondMethod()
+        {
+            Console.Write("Please input width: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("Please input length: ");
+            int length = int.Parse(Console.ReadLine());
+            double area = width * length;
+            Console.WriteLine($"The area of your rectangle is {area}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IndividualWork_FranciscoVelazquez07292020/Program.cs
@@ -0,0 +1,133 @@
+﻿/*
+Class:        ISTA220 Programming Fundamentals
+Student:      Francisco Velazquez
+Instructor:   Christine Lee
+Date:         07/29/2020
+Descrption:   This application asks you for a number from 1 to 7 that is in line with your favorite day of the week. It performs this function either with if else statememts or with a switch.
+Revised:      
+Revision By:  
+Revision:     
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualWork_07292020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Christine!");
+            Console.WriteLine("There are 2 methods to this app. Please 1 for method one and 2 for method two");
+            int method = 0;
+            while (!int.TryParse(Console.ReadLine(), out method))
+            {
+                Console.Write("Please enter a number: ");
+            }
+            while(method < 1 | method > 2)
+            {
+                Console.WriteLine("Please enter 1 for method one or 2 for method 2");
+                Console.ReadLine();
+            }
+            if (method is 1)
+            {
+                Console.WriteLine("You have chosen method one!");
+            }
+            else if (method is 2)
+            {
+                Console.WriteLine("You have chosen method two!");
+                Program p = new Program();
+                p.methodtwo();
+            }
+
+            //Method one: Favorite day using if else statements
+            Console.WriteLine("Please input a number from 1 to 7 that will represent your favorite day of the week.");
+            int weekday = 0;
+            while (!int.TryParse(Console.ReadLine(), out weekday))
+            {
+                Console.Write("Please enter a number: ");
+            }
+            while (weekday < 1 | weekday > 7)
+            {
+                Console.WriteLine("Please enter number between 1 and 7");
+                Console.ReadLine();
+            }
+            if (weekday is 1)
+            {
+                Console.WriteLine("Your favorite day is Sunday!");
+            }
+            else if (weekday is 2)
+            {
+                Console.WriteLine("Your favorite day is Monday!");
+            }
+            else if (weekday is 3)
+            {
+                Console.WriteLine("Your favorite day is Tuesday!");
+            }
+            else if (weekday is 4)
+            {
+                Console.WriteLine("Your favorite day is Wednesday!");
+            }
+            else if (weekday is 5)
+            {
+                Console.WriteLine("Your favorite day is Thursday!");
+            }
+            else if (weekday is 6)
+            {
+                Console.WriteLine("Your favorite day is Friday!");
+            }
+            else if (weekday is 7)
+            {
+                Console.WriteLine("Your favorite day is Saturday!");
+            }
+        }
+
+        public void methodtwo()
+        {
+            //Method two: Favorite day using switch statement
+            Console.WriteLine("Please input a number from 1 to 7 that will represent your favorite day of the week.");
+            int Weekday = 0;
+            while (!int.TryParse(Console.ReadLine(), out Weekday))
+            {
+                Console.Write("Please enter a number: ");
+            }
+            while (Weekday < 1 | Weekday > 7)
+            {
+                Console.WriteLine("Please enter number between 1 and 7");
+                Console.ReadLine();
+            }
+            switch (Weekday)
+            {
+                case 1:
+                    Console.WriteLine("Your favorite day is Sunday!");
+                    break;
+                case 2:
+                    Console.WriteLine("Your favorite day is Monday!");
+                    break;
+                case 3:
+                    Console.WriteLine("Your favorite day is Tuesday!");
+                    break;
+                case 4:
+                    Console.WriteLine("Your favorite day is Wednesday!");
+                    break;
+                case 5:
+                    Console.WriteLine("Your favorite day is Thursday!");
+                    break;
+                case 6:
+                    Console.WriteLine("Your favorite day is Friday!");
+                    break;
+                case 7:
+                    Console.WriteLine("Your favorite day is Saturday!");
+                    break;
+                default:
+                    Console.WriteLine("You didn't choose a day! =(");
+                    break;
+            }
+            Environment.Exit(0);
+        }
+    }
+}
